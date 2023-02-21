a1020f71f3fe9ce3b382a342cf1eaeddc8d47544
cosmoquester <cosmoquester@gmail.com>
2023-02-21 21:45:37 +0900

    build: Add torch to requirements

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     description="This repository is template for my python project.",
     python_requires='>=3.7',
-    install_requires=[],
+    install_requires=["torch"],
     url="https://github.com/cosmoquester/general-memory-network.git",
     author="Park Sangjun",
     packages=find_packages(exclude=["tests"]),

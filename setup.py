b53120f4acdb1e94d6b40849b194a18d0f891a6d
cosmoquester <cosmoquester@gmail.com>
2023-02-23 20:39:36 +0900

    build: Rename package and url

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="general-memory-network",
+    name="memoria",
     version="0.0.1",
     description="This repository is template for my python project.",
-    python_requires='>=3.7',
+    python_requires=">=3.7",
     install_requires=["torch"],
-    url="https://github.com/cosmoquester/general-memory-network.git",
+    url="https://github.com/cosmoquester/memoria.git",
     author="Park Sangjun",
     packages=find_packages(exclude=["tests"]),
 )

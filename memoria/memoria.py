7e5d4da1afe6649924fe741dbbcd6c5d768e36a0
cosmoquester <cosmoquester@gmail.com>
2023-04-13 09:00:35 +0900

    feat: Use dot product weight

--- memoria/memoria.py
+++ memoria/memoria.py
@@ -116,8 +116,9 @@ class Memoria:
         Returns:
             attention weights shaped [BatchSize, WorkingMemoryLength, ShorttermMemoryLength]
         """
-        l2_square = (working_memory.data.unsqueeze(2) - key_memory.data.unsqueeze(1)).pow(2).sum(dim=3)
-        return l2_square.neg().exp()
+        weight = working_memory.data @ key_memory.data.transpose(1, 2)
+        weight = weight.softmax(dim=2)
+        return weight
 
     @torch.no_grad()
     def _remind_shortterm_memory(self, weight: torch.Tensor, shortterm_memory_indices: torch.Tensor) -> torch.Tensor:

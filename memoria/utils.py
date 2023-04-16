2e7052c2e98cd5e5bc26b3eec74bba65686f3fb7
cosmoquester <cosmoquester@gmail.com>
2023-04-17 02:44:45 +0900

    fix: Edge cases for `super_unique`

--- memoria/utils.py
+++ memoria/utils.py
@@ -2,6 +2,9 @@ import torch
 
 
 def super_unique(t: torch.Tensor, dim: int) -> torch.Tensor:
+    if t.numel() == 0:
+        return t
+
     min_value = t.min()
     t = t - min_value
 
@@ -11,7 +14,8 @@ def super_unique(t: torch.Tensor, dim: int) -> torch.Tensor:
     unique_t_mask = torch.zeros(new_shape, dtype=torch.bool, device=t.device)
     unique_t_mask.scatter_(dim, t.long(), True)
 
-    validity, unique_t = unique_t_mask.int().topk(t.size(dim), dim=dim)
+    k = min(t.size(dim), unique_t_mask.size(dim))
+    validity, unique_t = unique_t_mask.int().topk(k, dim=dim)
     unique_t += min_value
     unique_t.masked_fill_(~validity.bool(), -1)
     return unique_t

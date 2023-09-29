867561c6d0e568036253db3d450c35d906fb7929
cosmoquester <cosmoquester@gmail.com>
2023-09-29 14:37:26 +0900

    feat: Add memoria roberta model

---
+++ experiment/longseq_formers/model/memoria_roberta/__init__.py
@@ -0,0 +1,8 @@
+from .configuration_memoria_roberta import MemoriaRobertaConfig
+from .modeling_memoria_roberta import MemoriaRobertaForSequenceClassification, MemoriaRobertaModel
+
+__all__ = [
+    "MemoriaRobertaConfig",
+    "MemoriaRobertaForSequenceClassification",
+    "MemoriaRobertaModel",
+]

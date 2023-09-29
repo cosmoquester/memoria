a12da450c3a9f5298b3465ec59e7e9cff78d41ac
cosmoquester <cosmoquester@gmail.com>
2023-09-29 14:36:39 +0900

    feat: Add compressive transformer model

--- experiment/longseq_formers/model/__init__.py
+++ experiment/longseq_formers/model/__init__.py
@@ -1,8 +1,12 @@
+from .compressive_former import CompressiveFormerConfig, CompressiveFormerLMHeadModel, CompressiveFormerModel
 from .gpt2_with_memoria import GPT2WithMemoriaConfig, GPT2WithMemoriaLMHeadModel, GPT2WithMemoriaModel
 from .infinity_gpt2 import InfinityGPT2Config, InfinityGPT2LMHeadModel, InfinityGPT2Model
 from .memoria_bert import MemoriaBertConfig, MemoriaBertForSequenceClassification, MemoriaBertModel
 
 __all__ = [
+    "CompressiveFormerConfig",
+    "CompressiveFormerLMHeadModel",
+    "CompressiveFormerModel",
     "GPT2WithMemoriaConfig",
     "GPT2WithMemoriaLMHeadModel",
     "GPT2WithMemoriaModel",

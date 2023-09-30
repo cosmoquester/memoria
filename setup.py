6b70a77f2426df5dfb042e3ecb0c8a7e2d6e1ca6
cosmoquester <cosmoquester@gmail.com>
2023-09-30 18:11:11 +0900

    build: Add build informations

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as f:
+    long_description = f.read()
+
 setup(
     name="memoria",
     version="0.0.1",
-    description="This repository is template for my python project.",
+    description="Memoria is a Hebbian memory architecture for neural networks.",
+    long_description=long_description,
     python_requires=">=3.7",
     install_requires=["torch"],
     url="https://github.com/cosmoquester/memoria.git",
     author="Park Sangjun",
+    keywords=["memoria", "hebbian", "memory", "transformer"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
     packages=find_packages(exclude=["tests", "experiment"]),
 )

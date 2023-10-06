88a85c05fefe7c660a578c9499addadb8c807603
Sangjun Park <cosmoquester@gmail.com>
2023-10-06 11:34:32 +0900

    build: Fix setup.py format errors

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
-    name="memoria",
+    name="memoria-pytorch",
     version="1.0.0",
     description="Memoria is a Hebbian memory architecture for neural networks.",
     long_description=long_description,
+    long_description_content_type="text/markdown",
     python_requires=">=3.7",
     install_requires=["torch"],
     url="https://github.com/cosmoquester/memoria.git",

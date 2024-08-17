4a47b290bd16e99b881ea2ed22cbec2820344e77
Sangjun Park <cosmoquester@gmail.com>
2024-08-17 22:52:54 +0900

    feat: Add history manager to keep engram history

--- memoria/__init__.py
+++ memoria/__init__.py
@@ -1,8 +1,9 @@
 from . import utils
 from .abstractor import Abstractor
 from .engram import Engrams, EngramType
+from .history_manager import HistoryManager
 from .memoria import Memoria
 from .sparse_tensor import SparseTensor
 
-__all__ = ["utils", "Abstractor", "Engrams", "EngramType", "Memoria", "SparseTensor"]
+__all__ = ["utils", "Abstractor", "Engrams", "EngramType", "HistoryManager", "Memoria", "SparseTensor"]
 __version__ = "1.0.0"
